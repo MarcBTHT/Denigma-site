adf0d28054811f6572f1eae7342482940b65acd2
Ewan Hamon <ewan.hamon@gmail.com>
2023-12-07 15:33:43 +0100

    Update Page2

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -105,7 +105,7 @@ function Page2() {
         <h1>Page 2</h1>
         <Link to="/page3">Page 3</Link>
         <div className={styles['bottom']}>
-          <Link to="/page3"><div className={styles['admin']} onClick={top}>admin</div></Link>
+          {/* <Link to="/page3"><div className={styles['admin']} onClick={top}>admin</div></Link> */}
           <Link to="/page3"><div className={styles['page3']} onClick={top}>Page 3</div></Link>
 
         </div>
