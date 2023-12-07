d2ff14cbc4b71dfc06d9db61f6620243feef0f20
Ewan Hamon <ewan.hamon@gmail.com>
2023-12-07 15:26:35 +0100

    Update Page2

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -102,9 +102,10 @@ function Page2() {
         <div className={styles['slide-169-3-item']} />
         <div className={styles['slide-169-3-inner']} />
         <b className={styles['lotterie-001']}>Sweepstake #001</b>
+        <h1>Page 2</h1>
+        <Link to="/page3">Page 3</Link>
         <div className={styles['bottom']}>
-          <h1>Page 2</h1>
-          <Link to="/page3">Page 3</Link>
+
         </div>
 
       </div>
